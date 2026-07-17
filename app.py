e5d5831c5e6a41999d46afdb10e1c1f7fe58c24e
Pooja <poojaumesh1912@gmail.com>
2026-07-17 15:42:18 +0530

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Hello from the CI/CD pipeline! Deployed automatically by Jenkins. 123\n"
+   return "Hello from the CI/CD pipeline! Deployed automatically by Jenkins. 12345\n"
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000)
